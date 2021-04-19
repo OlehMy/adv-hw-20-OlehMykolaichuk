d1c45aefe12ac6411f6ea971db7216b524b3d3b6
Oleh Mykolaichuk <ooolehmy@gmail.com>
2021-04-19 21:27:43 +0300

    First version

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Timer from './components/Timer';
+import './components/style.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Timer/>
+    </div>
+  );
+}
+
+export default App;
